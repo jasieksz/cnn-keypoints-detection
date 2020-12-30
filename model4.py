e6f36bd2bcbe4dbec5ccb1c96cad8f8db8984c2d
Jasiek Sznajd <jasiek.sznajd@gmail.com>
2020-12-30 20:56:50 +0100

    ResNet50 and InceptionResNet2

--- model4.py
+++ model4.py
@@ -6,13 +6,16 @@ import numpy as np
 from numpy.lib.npyio import load
 import pandas as pd
 import matplotlib.pyplot as plt
+from typing import List
+from functools import reduce
 
 import tensorflow as tf
 from tensorflow.keras.backend import flatten, sum
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, UpSampling2D, Concatenate
-from tensorflow.keras.losses import binary_crossentropy
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, UpSampling2D, Concatenate, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.losses import mean_absolute_error
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.applications import ResNet50
 
 #%%[markdown]
 ### Run config
@@ -34,14 +37,14 @@ config
 #%%[markdown]
 ### Helper methods
 #
-def loadData(path:str, labels:list, imgSize:tuple, validationSubset:bool=False) -> tf.data.Dataset:
+def loadData(path:str, labels:list, imgSize:tuple, colorMode:str='grayscale', validationSubset:bool=False) -> tf.data.Dataset:
     batch_size = 32
     subset = "validation" if validationSubset else "training"
 
     return tf.keras.preprocessing.image_dataset_from_directory(
         path,
         labels=labels,
-        color_mode="grayscale",
+        color_mode=colorMode,
         image_size=imgSize,
         batch_size=batch_size,
         validation_split=0.2,
@@ -49,22 +52,22 @@ def loadData(path:str, labels:list, imgSize:tuple, validationSubset:bool=False) 
         seed=123
     )
 
-def loadTestData(path:str, labels:list, imgSize:tuple) -> tf.data.Dataset:
+def loadTestData(path:str, labels:list, imgSize:tuple, colorMode:str='grayscale') -> tf.data.Dataset:
     batch_size = 32
     return tf.keras.preprocessing.image_dataset_from_directory(
         path,
         labels=labels,
-        color_mode="grayscale",
+        color_mode=colorMode,
         image_size=imgSize,
         batch_size=batch_size,
     )
 
-def loadImagesFromDir(basePath, imgSize:tuple):
+def loadImagesFromDir(basePath, imgSize:tuple, colorMode:str='grayscale'):
     imagePaths = [basePath + imageName for imageName in os.listdir(basePath)]
     return [tf.keras.preprocessing.image.img_to_array(img) 
             for img in [tf.keras.preprocessing.image.load_img(
                     imagePath,
-                    color_mode='grayscale', 
+                    color_mode=colorMode, 
                     target_size=imgSize) for imagePath in imagePaths]]
 
 def plotObjectsWithKeypoints(slice:int, images:list, labels:list, yPred:np.ndarray) -> None:
@@ -77,11 +80,9 @@ def plotObjectsWithKeypoints(slice:int, images:list, labels:list, yPred:np.ndarr
         plt.axis("off")
     plt.show()
 
-def plotTrainingHistory(history:tf.keras.callbacks.History, metric:str) -> None:
-    loss = history.history[metric]
-    val_loss = history.history['val_{}'.format(metric)]
-
-    epochs_range = range(epochs)
+def plotTrainingHistory(histories:List[tf.keras.callbacks.History], metric:str, epochs_range) -> None:
+    loss = reduce(lambda a,b: a+b, [h.history[metric] for h in histories])
+    val_loss = reduce(lambda a,b: a+b, [h.history['val_{}'.format(metric)] for h in histories])
 
     plt.figure(figsize=(8, 8))
     plt.plot(epochs_range, loss, label='Training {}'.format(metric))
@@ -95,34 +96,31 @@ def plotTrainingHistory(history:tf.keras.callbacks.History, metric:str) -> None:
 #
 testDataSize = np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).shape[0]
 trainDataSize = np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).shape[0]
-imageH = 160
-imageW = 160
+imageH = 128
+imageW = 128
 numFeatures = 16
 
 dsTrain = loadData(path='resources/{}/train'.format(objectName),
             labels=list(np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).reshape(trainDataSize, numFeatures)),
             imgSize=(imageH,imageW),
+            colorMode='rgb',
             validationSubset=False)
 
 dsValid = loadData(path='resources/{}/train'.format(objectName),
                     labels=list(np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).reshape(trainDataSize,numFeatures)),
                     imgSize=(imageH,imageW),
+                    colorMode='rgb',
                     validationSubset=True)
 
 dsTest = loadTestData(path='resources/{}/test'.format(objectName),
                     labels=list(np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).reshape(testDataSize, numFeatures)),
+                    colorMode='rgb',
                     imgSize=(imageH,imageW))
 
 
 #%%[markdown]
 ### Configure performance 
 #
-# ```
-# data_augmentation = tf.keras.Sequential([
-#     layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-# ])
-# .map(lambda x, y: (data_augmentation(x, training=True), y), num_parallel_calls=AUTOTUNE) \
-# ```
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -136,99 +134,66 @@ dsValid = dsValid.cache() \
 #%%[markdown]
 ### Create basic model
 #
-def dice_loss(y_true, y_pred):
-    smooth = 1.
-    y_true_f = flatten(y_true)
-    y_pred_f = flatten(y_pred)
-    intersection = sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (sum(y_true_f) + sum(y_pred_f) + smooth)
-
-
-def bce_dice_loss(y_true, y_pred):
-    return binary_crossentropy(y_true, y_pred) + (1 - dice_loss(y_true, y_pred))
-
-#%%
-inputs = Input((imageH, imageW))
-conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-drop4 = Dropout(0.5)(conv4)
-pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-conv5 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-conv5 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-drop5 = Dropout(0.5)(conv5)
-
-up6 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-merge6 = Concatenate(3)([drop4,up6])
-conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-merge7 = Concatenate(3)([conv3,up7])
-conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-merge8 = Concatenate(3)([conv2,up8])
-conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-merge9 = Concatenate(3)([conv1, up9])
-conv9 = Conv2D(48, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-conv9 = Conv2D(48, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-# conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-conv10 = Dense(numFeatures)(conv9)
-
-model = Model(inputs = inputs, outputs = conv10)
-
-model.compile(optimizer = Adam(lr = 1e-3), loss=bce_dice_loss, metrics=[dice_loss, 'accuracy'])
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(imageH, imageW, 3))
+ 
+for layer in base_model.layers:
+    layer.trainable = True
+ 
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(16)(x) 
+ 
+model = Model(inputs=[base_model.input], outputs=[predictions])
+ 
+model.compile(optimizer=Adam(learning_rate=0.0005),
+            loss="mean_squared_error",
+            metrics=['mae'])
+ 
 model.summary()
 
+#%%
+histories = []
+totalEpochs = 0
 
 #%%[markdown]
 ### Train the model
 #
-epochs = 10
+epochs = 1000
 history = model.fit(dsTrain,
                 validation_data=dsValid,
                 epochs=epochs)
 
-#%%[markdown]
-### Inspect training accuracy
-#
-plotTrainingHistory(history, metric='loss')
-plotTrainingHistory(history, metric='accuracy')
+histories.append(history)
+totalEpochs += epochs
 
-#%%[markdown]
-### Accuracy score model
-#
+
+plotTrainingHistory(histories, metric='loss', epochs_range=range(totalEpochs))
+plotTrainingHistory(histories, metric='mae', epochs_range=range(totalEpochs))
+
+print("EVALUATE")
 test_loss, test_acc = model.evaluate(dsTest, verbose=2)
 
 #%%[markdown]
 ## Predict
 #
 yPred = model.predict(dsTest)
-
-#%%
-images = loadImagesFromDir('resources/{}/test/masks/'.format(objectName), (imageH, imageW))
+images = loadImagesFromDir('resources/{}/test/masks/'.format(objectName), (imageH, imageW), 'rgb')
 labels = list(np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).reshape(testDataSize,numFeatures))
 
 #%%[markdown]
 ## Plot prediction results
 #
-plotObjectsWithKeypoints(slice=1, images=images, labels=labels, yPred=yPred)
-plotObjectsWithKeypoints(slice=4, images=images, labels=labels, yPred=yPred)
+a = list(zip(images, labels, yPred))
+a.sort(key=lambda t: mean_absolute_error(t[1], t[2]).numpy())
+a = np.array(a)
+
+print("BEST\n")
+plotObjectsWithKeypoints(slice=0, images=a[:,0], labels=a[:,1], yPred=a[:,2])
+
+print("\nWORST\n")
+plotObjectsWithKeypoints(slice=6, images=a[:,0], labels=a[:,1], yPred=a[:,2])
+
 
 #%%[markdown]
 ## Save model
@@ -236,3 +201,5 @@ plotObjectsWithKeypoints(slice=4, images=images, labels=labels, yPred=yPred)
 if saveModel:
     model.save('resources/models/' + modelName)
 
+#%%
+model.save('resources/models/ResNet50-1010e')
--- model5.py
+++ model5.py
@@ -6,19 +6,24 @@ import numpy as np
 from numpy.lib.npyio import load
 import pandas as pd
 import matplotlib.pyplot as plt
+from typing import List
+from functools import reduce
 
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.backend import flatten, sum
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, UpSampling2D, Concatenate, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.losses import mean_absolute_error
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.applications import ResNet50, InceptionResNetV2
 
 #%%[markdown]
 ### Run config
 #
 
 saveModel = False
-objectName = "duck"
-modelName = "model-{}-{}".format(objectName,31)
+objectName = "drill"
+modelName = "model-{}-{}".format(objectName,51)
 
 #%%[markdown]
 ### Configure GPU and CUDA
@@ -32,14 +37,14 @@ config
 #%%[markdown]
 ### Helper methods
 #
-def loadData(path:str, labels:list, imgSize:tuple, validationSubset:bool=False) -> tf.data.Dataset:
+def loadData(path:str, labels:list, imgSize:tuple, colorMode:str='grayscale', validationSubset:bool=False) -> tf.data.Dataset:
     batch_size = 32
     subset = "validation" if validationSubset else "training"
 
     return tf.keras.preprocessing.image_dataset_from_directory(
         path,
         labels=labels,
-        color_mode="grayscale",
+        color_mode=colorMode,
         image_size=imgSize,
         batch_size=batch_size,
         validation_split=0.2,
@@ -47,22 +52,22 @@ def loadData(path:str, labels:list, imgSize:tuple, validationSubset:bool=False) 
         seed=123
     )
 
-def loadTestData(path:str, labels:list, imgSize:tuple) -> tf.data.Dataset:
+def loadTestData(path:str, labels:list, imgSize:tuple, colorMode:str='grayscale') -> tf.data.Dataset:
     batch_size = 32
     return tf.keras.preprocessing.image_dataset_from_directory(
         path,
         labels=labels,
-        color_mode="grayscale",
+        color_mode=colorMode,
         image_size=imgSize,
         batch_size=batch_size,
     )
 
-def loadImagesFromDir(basePath, imgSize:tuple):
+def loadImagesFromDir(basePath, imgSize:tuple, colorMode:str='grayscale'):
     imagePaths = [basePath + imageName for imageName in os.listdir(basePath)]
     return [tf.keras.preprocessing.image.img_to_array(img) 
             for img in [tf.keras.preprocessing.image.load_img(
                     imagePath,
-                    color_mode='grayscale', 
+                    color_mode=colorMode, 
                     target_size=imgSize) for imagePath in imagePaths]]
 
 def plotObjectsWithKeypoints(slice:int, images:list, labels:list, yPred:np.ndarray) -> None:
@@ -75,11 +80,9 @@ def plotObjectsWithKeypoints(slice:int, images:list, labels:list, yPred:np.ndarr
         plt.axis("off")
     plt.show()
 
-def plotTrainingHistory(history:tf.keras.callbacks.History, metric:str) -> None:
-    loss = history.history[metric]
-    val_loss = history.history['val_{}'.format(metric)]
-
-    epochs_range = range(epochs)
+def plotTrainingHistory(histories:List[tf.keras.callbacks.History], metric:str, epochs_range) -> None:
+    loss = reduce(lambda a,b: a+b, [h.history[metric] for h in histories])
+    val_loss = reduce(lambda a,b: a+b, [h.history['val_{}'.format(metric)] for h in histories])
 
     plt.figure(figsize=(8, 8))
     plt.plot(epochs_range, loss, label='Training {}'.format(metric))
@@ -93,34 +96,31 @@ def plotTrainingHistory(history:tf.keras.callbacks.History, metric:str) -> None:
 #
 testDataSize = np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).shape[0]
 trainDataSize = np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).shape[0]
-imageH = 160
-imageW = 160
+imageH = 128
+imageW = 128
 numFeatures = 16
 
 dsTrain = loadData(path='resources/{}/train'.format(objectName),
             labels=list(np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).reshape(trainDataSize, numFeatures)),
             imgSize=(imageH,imageW),
+            colorMode='rgb',
             validationSubset=False)
 
 dsValid = loadData(path='resources/{}/train'.format(objectName),
                     labels=list(np.load('resources/{}/{}_keypoints_train.npy'.format(objectName, objectName)).reshape(trainDataSize,numFeatures)),
                     imgSize=(imageH,imageW),
+                    colorMode='rgb',
                     validationSubset=True)
 
 dsTest = loadTestData(path='resources/{}/test'.format(objectName),
                     labels=list(np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).reshape(testDataSize, numFeatures)),
+                    colorMode='rgb',
                     imgSize=(imageH,imageW))
 
 
 #%%[markdown]
 ### Configure performance 
 #
-# ```
-# data_augmentation = tf.keras.Sequential([
-#     layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-# ])
-# .map(lambda x, y: (data_augmentation(x, training=True), y), num_parallel_calls=AUTOTUNE) \
-# ```
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -134,65 +134,69 @@ dsValid = dsValid.cache() \
 #%%[markdown]
 ### Create basic model
 #
-
-model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(imageH, imageW, 1)),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(128, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(128, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.2),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(numFeatures)
-])
-
-model.compile(optimizer='adam',
-              loss='mean_squared_error',
-              metrics=['mae'])
-
-
+base_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(imageH, imageW, 3))
+ 
+for layer in base_model.layers:
+    layer.trainable = True
+ 
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(16)(x) 
+ 
+model = Model(inputs=[base_model.input], outputs=[predictions])
+ 
+model.compile(optimizer=Adam(learning_rate=0.0001),
+            loss="mean_squared_error",
+            metrics=['mae'])
+ 
 model.summary()
+
+#%%
+histories = []
+totalEpochs = 0
 
 #%%[markdown]
 ### Train the model
 #
-epochs = 250
+epochs = 500
 history = model.fit(dsTrain,
                 validation_data=dsValid,
                 epochs=epochs)
 
-#%%[markdown]
-### Inspect training accuracy
-#
-plotTrainingHistory(history, metric='loss')
-plotTrainingHistory(history, metric='accuracy')
+histories.append(history)
+totalEpochs += epochs
 
-#%%[markdown]
-### Accuracy score model
-#
+#%%
+plotTrainingHistory(histories, metric='loss', epochs_range=range(totalEpochs))
+plotTrainingHistory(histories, metric='mae', epochs_range=range(totalEpochs))
+
+print("EVALUATE")
 test_loss, test_acc = model.evaluate(dsTest, verbose=2)
 
 #%%[markdown]
 ## Predict
 #
 yPred = model.predict(dsTest)
-
-#%%
-images = loadImagesFromDir('resources/{}/test/masks/'.format(objectName), (imageH, imageW))
+images = loadImagesFromDir('resources/{}/test/masks/'.format(objectName), (imageH, imageW), 'rgb')
 labels = list(np.load('resources/{}/{}_keypoints_test.npy'.format(objectName, objectName)).reshape(testDataSize,numFeatures))
 
 #%%[markdown]
 ## Plot prediction results
 #
-plotObjectsWithKeypoints(slice=1, images=images, labels=labels, yPred=yPred)
-plotObjectsWithKeypoints(slice=4, images=images, labels=labels, yPred=yPred)
+a = list(zip(images, labels, yPred))
+a.sort(key=lambda t: mean_absolute_error(t[1], t[2]).numpy())
+a = np.array(a)
+
+print("BEST\n")
+plotObjectsWithKeypoints(slice=0, images=a[:,0], labels=a[:,1], yPred=a[:,2])
+
+print("\nWORST\n")
+plotObjectsWithKeypoints(slice=6, images=a[:,0], labels=a[:,1], yPred=a[:,2])
+
 
 #%%[markdown]
 ## Save model
 #
 if saveModel:
     model.save('resources/models/' + modelName)
-
